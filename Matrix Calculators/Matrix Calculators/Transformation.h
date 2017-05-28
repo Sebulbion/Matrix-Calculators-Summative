245967709351cbf317bb749de0696c7f32b99ac1
sebastian.ten7230 <sebastian.ten7230@1808C004>
2017-05-28 16:29:47 +1200

    Finished Transformation excluding projection

--- Matrix Calculators/Matrix Calculators/Transformation.h
+++ Matrix Calculators/Matrix Calculators/Transformation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Windows.h>
+#include <math.h>
 #include "resource.h"
 
 class CTransformation
@@ -12,27 +13,25 @@ public:
 
 	//void SetResultantMat(HWND _hDlg);
 	void ApplyScaling(HWND _hDlg);
-	void CreateIdentatyMatrix(float _rgMatrix[4][4]);
+	void ApplyTranslation(HWND _hDlg);
+	void ApplyRotation(HWND _hDlg);
+	void PrintIdentatyMatrix(HWND _hDlg);
 
 private:
 
 	CTransformation();
 	CTransformation(const CTransformation& _kr);
 	CTransformation& operator=(const CTransformation& _kr);
+
 	void WriteToDialogBoxCMFormat(HWND _hDlg);
-
+	void WriteToDialogBoxRMFormat(HWND _hDlg);
+	void CreateIdentatyMatrix(float _rgMatrix[4][4]);
 	void MultiplyMatrix(HWND _hDlg, float _rgInputMat[4][4]);
-	float m_rgfResultantMat[4][4] = {
-		{ 1,0,0,0 },
-		{ 0,1,0,0 },
-		{ 0,0,1,0 },
-		{ 0,0,0,1 } };
+	void SetMatrix(float _rgMatrixA[4][4], float _rgMatrixB[4][4]);
 
-	float m_rgfTempMat[4][4] = {
-		{ 1,0,0,0 },
-		{ 0,1,0,0 },
-		{ 0,0,1,0 },
-		{ 0,0,0,1 } };
+	float m_rgfResultantMat[4][4];
+	float m_rgfUserInputMat[4][4];
+	float m_rgfTempMat[4][4];
 
 protected:
 	static CTransformation* s_pTransformation;
